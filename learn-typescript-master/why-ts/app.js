d7f39297442d4e054e8da55a81d1544ce5d84576
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-01-18 16:45:26 -0800

    typescript advantage #1

--- learn-typescript-master/why-ts/app.js
+++ learn-typescript-master/why-ts/app.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 // api url
 var url = 'https://jsonplaceholder.typicode.com/users/1';
 
@@ -9,13 +11,44 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+// JS DOC standard grammer, generic = < >  this part.
+
+/**
+ * @typedef {object} Address 
+ * @property {string} street 
+ * @property {string} city 
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name 
+ * @property {string} email 
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url)
+}
+
+fetchUser().then(function(response) {
+  response.address.street
+})
+
 function startApp() {
-  axios
-    .get(url)
+  //axios
+    //.get(url)
+    fetchUser()
     .then(function (response) {
-      console.log(response);
+      //console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
