e0dbcbbd54f3eb1f9324d8e6a0a5955585a63bdb
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-01-18 17:16:38 -0800

    use TypeScript by JavaScript

--- learn-typescript-master/why-ts/sample.js
+++ learn-typescript-master/why-ts/sample.js
@@ -1,7 +1,23 @@
+//function sum(a, b) {
+//    return a + b;
+//}
+
+//sum(10, "20")  // 1020,  JS consider both arguments as a string becuase of a string "20"
+
+
+
+// use TypeScirpt by JavaScript
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * 
+ */
 function sum(a, b) {
     return a + b;
 }
 
-sum(10, "20")  // 1020,  JS consider both arguments as a string becuase of a string "20"
-
-sum()
+sum(10, '20')
