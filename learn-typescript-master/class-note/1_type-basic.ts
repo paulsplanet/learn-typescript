5c058729ee454a8816c7e3f464dc657d06e741a2
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-01-20 09:44:33 -0800

    TS basic types

---
+++ learn-typescript-master/class-note/1_type-basic.ts
@@ -0,0 +1,31 @@
+// JS declare string
+//let str = 'hello';
+
+// TS decalre string
+let str: string = 'hello';
+
+// TS number
+let numb: number = 10;
+
+// TS array
+let arr: Array<number> = [1,2,3]
+let heroes: Array<string> =['cap', 'thor', 'hulk', 20]
+let items: number[] = [4,5,6]
+
+
+// TS tuple
+let address: [string, number] = ['LA', 100];
+
+// TS object
+let obj: object = {};
+// let person: object = {
+//     name: 'paul',
+//     age: 30,
+// };
+let person: { name: string, age: number} = {
+    name: 'hulk',
+    age: 40,
+};
+
+// TS boolean
+let show: boolean = true;
