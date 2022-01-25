ed6ccbf28b4c7da492b90d19a6508ab5fac4f603
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-01-24 21:08:54 -0800

    interface #2

--- learn-typescript-master/class-note/3_interface.ts
+++ learn-typescript-master/class-note/3_interface.ts
@@ -19,3 +19,63 @@ const cap = {
     name: 'rogers',
 }
 getUser(cap);       // if I have only name in "cap", age is missing so error.
+
+// const cap ={
+//     name: 'rogers',
+//     age: 28,
+// }
+// getUser(cap);       this is the correct way
+
+// use interface for structure of function
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+let hab: SumFunction;
+hab = function(a: number, b: number): number {
+    return a + b;
+}
+
+//  indexing
+interface StringArray {
+    [index: number]: string
+}
+
+let arr: StringArray = ['a', 'b', 'c'];
+arr[0];     // 'a'
+arr[1] = 10;    // 10 is not a string => error
+
+// Dictionary Pattern
+interface StringRegexDictionary {
+    [key: string]: RegExp;
+}
+
+let obj: StringRegexDictionary = {
+    // sth: /abc/,
+    // cssFile: 'css'  //  right side has to be "RegExp" by Dictionary Pattern
+    cssFileTwo: /\.css$/,
+    jsFile: /\.js$/,
+}
+
+Object.keys(obj).forEach(function(value){
+
+})
+
+//  interface extension
+interface Person {
+    name: string;
+    age: number;
+}
+
+// interface Developer {
+//     name: string;
+//     age: number;
+//     language: string;
+// }   =>
+interface Developer extends Person {
+    language: string;
+}
+
+var captain: Developer = {
+    language: 'eng',
+}                               // missing properties of name & age
