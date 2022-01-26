5a686165dd04e7a93702a270dae45f1cd3a3df80
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-01-25 16:54:24 -0800

    intersection type

--- learn-typescript-master/class-note/5_operator.ts
+++ learn-typescript-master/class-note/5_operator.ts
@@ -24,3 +24,32 @@ function typeMessage(value: string | number) {
 }
 
 typeMessage('hello')
+
+/* --------------------------------------------------------------------------------- */
+interface Develop {
+    name: string;
+    skill: string;
+}
+
+interface Human {
+    name: string;
+    age: number;
+}
+
+function askSomeone(someone: Develop | Human) {
+    someone.name;            // when I use union function of two interface, only common property is accessable
+                            // it is name here. when I type someone.  then it shows only name (no, skill and age)
+    someone.skill           // this is not a common property of Develop and Human => error             
+}
+askSomeone({ name: 'paul', skill: 'developing' });
+askSomeone({ name: 'daekun', age: 25 })
+
+
+// Intersection type, symbal is  '  &  '
+function askHim(someone: Develop & Human) {
+    someone.name;
+    someone.skill;
+    someone.age;                    // Intersection type contains both Develop & Human properties
+}
+
+askHim({ name: 'paul', skill: 'developing' })       // age is missing => error
