1a5148edf4cc76f9397db636ece3b57159b3e593
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-02-07 20:03:11 -0800

    use generic in interface - example

--- learn-typescript-master/example/dropdown-generic.ts
+++ learn-typescript-master/example/dropdown-generic.ts
@@ -1,26 +1,99 @@
-interface Email {
-  value: string;
+//    use TS
+// const emails: { value: string; selected: boolean }[] = [
+//   { value: 'naver.com', selected: true },
+//   { value: 'gmail.com', selected: false },
+//   { value: 'hanmail.net', selected: false },
+// ];
+
+// const numberOfProducts: { value: number; selected: boolean}[] = [
+//   { value: 1, selected: true },
+//   { value: 2, selected: false },
+//   { value: 3, selected: false },
+// ];
+
+// function createDropdownItem(item: { value: string; selected: boolean } | { value: number; selected: boolean } ) {
+//   const option = document.createElement('option');
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+// // NOTE: 이메일 드롭 다운 아이템 추가
+// emails.forEach(function (email) {
+//   const item = createDropdownItem(email);
+//   const selectTag = document.querySelector('#email-dropdown');
+//   selectTag.appendChild(item);
+// });
+
+// numberOfProducts.forEach(function (product) {
+//   const item = createDropdownItem(product);
+// })
+
+
+
+//    use interface
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+// const emails: Email[] = [
+//   { value: 'naver.com', selected: true },
+//   { value: 'gmail.com', selected: false },
+//   { value: 'hanmail.net', selected: false },
+// ];
+
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+// const numberOfProducts: ProductNumber[] = [
+//   { value: 1, selected: true },
+//   { value: 2, selected: false },
+//   { value: 3, selected: false },
+// ];
+
+// function createDropdownItem(item: Email | ProductNumber) {
+//   const option = document.createElement('option');
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+// // NOTE: 이메일 드롭 다운 아이템 추가
+// emails.forEach(function (email) {
+//   const item = createDropdownItem(email);
+//   const selectTag = document.querySelector('#email-dropdown');
+//   selectTag.appendChild(item);
+// });
+
+// numberOfProducts.forEach(function (product) {
+//   const item = createDropdownItem(product);
+// })
+
+
+/*  Use generic with interface  */
+interface DropdownItem<T> {
+  value: T;
   selected: boolean;
 }
 
-const emails: Email[] = [
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
-
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -38,3 +111,25 @@ emails.forEach(function (email) {
 numberOfProducts.forEach(function (product) {
   const item = createDropdownItem(product);
 })
+
+/*    Use generic one more to not use union
+
+function createDropdownItem<T>(item: DropdownItem<T>) {
+  const option = document.createElement('option');
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
+}
+
+// NOTE: 이메일 드롭 다운 아이템 추가
+emails.forEach(function (email) {
+  const item = createDropdownItem<string>(email);
+  const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem<number>(product);
+})
+*/
