df9019063b1ec2e4cf8286ea89b7a7a31d3e5bbb
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-02-14 10:27:21 -0800

    TS_type compatibility

--- learn-typescript-master/class-note/12_type-compatibility.ts
+++ learn-typescript-master/class-note/12_type-compatibility.ts
@@ -1,4 +1,4 @@
-//  Structural Typing
+//  Structural Typing, Type compatibility = interface & class
 
 interface Developer {
     name: string;
@@ -21,3 +21,35 @@ class Ingan {
 develop = new Ingan();  // if add skill in Ingan, then this works
 
 /* structural typing compare the inside parameters and its types */
+
+
+//  Structural Typing, Type compatibility = function & generic
+
+let add = function(a: number): number {
+    return a;
+}
+
+let sum = function(a: number, b: number): number {
+    return a + b;
+}
+
+add = sum;      // add take only one parameter, while sum takes two parameters
+sum = add;      // sum takes 2 while add takes 1.
+
+// generic
+interface Empty<T> {
+    
+}
+let empty1: Empty<string>;
+let empty2: Empty<number>;
+empty1 = empty2;
+empty2 = empty1;        // both directions are working
+
+
+interface NotEmpty<T> {
+    data: T;
+}
+let notEmpty1: NotEmpty<string>;
+let notEmpty2: NotEmpty<number>;
+notEmpty1 = notEmpty2;
+notEmpty2 = notEmpty1;      // date was assigned by each type already = cannot be same.
