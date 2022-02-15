27ca3fc4dd03dd0e2dc01dd3e3098fe1ed5d6a37
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2022-02-15 11:58:10 -0800

    TS _ Utility Type _ implementing Partial

--- learn-typescript-master/class-note/13_utility-type.ts
+++ learn-typescript-master/class-note/13_utility-type.ts
@@ -11,6 +11,7 @@ function fetchProduct(): Promise<Product[]> {
 
 }
 
+// Function to display detail infomation of aproduct
 //      1. use TS directly = cannot use interface Product becuase it is only partial info
 // function displayProductDetail(shoppingItem: { id: number; name: string; price: number }) {
 // }
@@ -29,5 +30,60 @@ function fetchProduct(): Promise<Product[]> {
 // }
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 function displayListByName(shoppingItem: ShoppingItem) {
-    
+
+}
+
+// Function to update product information
+//      1. user ?: symbol for optional
+// interface UpdateProduct {       
+//     id?: number;                // will update some information, not every items every times
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+// function updateProductItem(productItem: UpdateProduct) {
+// }
+
+//      2. use Partial uitility type
+type UpdateProduct = Partial<Product>;
+function updateProductItem(productItem: Partial<Product>) {
+
+}
+
+
+//  Utility Type _ Partial
+interface UserProfile {
+    username: string;
+    email: string;
+    profilePhotoUrl: string;
+}
+
+// interface UserProfileUpdate {
+//     username?: string;
+//     email?: string;
+//     profilePhotoUrl?: string;
+// }
+
+//  #1
+type UserProfileUpdate = {
+    username?: UserProfile['username']; 
+    email?: UserProfile['email'];
+    profilePhotoUrl?: UserProfile['profilePhotoUrl']; 
+}
+
+// #2 - mapped type?
+type UserProfileUpdate = {
+    [p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p]        // [x in xx] like for in loop
+}
+
+// #3
+type UserProfileKeys = keyof UserProfile;
+type UserProfileUpdate = {
+    [p in keyof UserProfile]?: UserProfile[p]
+}
+
+// #4
+type Subset<T> = {
+    [p in keyof T]?: T[p]
 }
